515a6455e5c10d1789868f3564cd3e86b7a87042
masikotimo <masikotimo@gmail.com>
2022-04-28 21:49:31 +0300

    initial commit

---
+++ src/app/shared/Sidebar.js
@@ -0,0 +1,406 @@
+import React, { Component } from "react";
+import { Link, withRouter } from "react-router-dom";
+import { Collapse } from "react-bootstrap";
+import { Trans } from "react-i18next";
+
+class Sidebar extends Component {
+  state = {};
+
+  toggleMenuState(menuState) {
+    if (this.state[menuState]) {
+      this.setState({ [menuState]: false });
+    } else if (Object.keys(this.state).length === 0) {
+      this.setState({ [menuState]: true });
+    } else {
+      Object.keys(this.state).forEach((i) => {
+        this.setState({ [i]: false });
+      });
+      this.setState({ [menuState]: true });
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.location !== prevProps.location) {
+      this.onRouteChanged();
+    }
+  }
+
+  onRouteChanged() {
+    document.querySelector("#sidebar").classList.remove("active");
+    Object.keys(this.state).forEach((i) => {
+      this.setState({ [i]: false });
+    });
+
+    const dropdownPaths = [
+      { path: "/apps", state: "appsMenuOpen" },
+      { path: "/basic-ui", state: "basicUiMenuOpen" },
+      { path: "/advanced-ui", state: "advancedUiMenuOpen" },
+      { path: "/form-elements", state: "formElementsMenuOpen" },
+      { path: "/tables", state: "tablesMenuOpen" },
+      { path: "/maps", state: "mapsMenuOpen" },
+      { path: "/icons", state: "iconsMenuOpen" },
+      { path: "/charts", state: "chartsMenuOpen" },
+      { path: "/user-pages", state: "userPagesMenuOpen" },
+      { path: "/error-pages", state: "errorPagesMenuOpen" },
+      { path: "/general-pages", state: "generalPagesMenuOpen" },
+      { path: "/ecommerce", state: "ecommercePagesMenuOpen" },
+    ];
+
+    dropdownPaths.forEach((obj) => {
+      if (this.isPathActive(obj.path)) {
+        this.setState({ [obj.state]: true });
+      }
+    });
+  }
+
+  render() {
+    return (
+      <nav className="sidebar sidebar-offcanvas" id="sidebar">
+        <ul className="nav">
+          <li className="nav-item nav-profile">
+            <a
+              href="!#"
+              className="nav-link"
+              onClick={(evt) => evt.preventDefault()}
+            >
+              <div className="nav-profile-image">
+                <img
+                  src={require("../../assets/images/faces/face1.jpg")}
+                  alt="profile"
+                />
+                <span className="login-status online"></span>{" "}
+                {/* change to offline or busy as needed */}
+              </div>
+              <div className="nav-profile-text">
+                <span className="font-weight-bold mb-2">
+                  <Trans>Timothy Masiko</Trans>
+                </span>
+                <span className="text-secondary text-small">
+                  <Trans>Fleet Manager</Trans>
+                </span>
+              </div>
+              <i className="mdi mdi-bookmark-check text-success nav-profile-badge"></i>
+            </a>
+          </li>
+          <li
+            className={
+              this.isPathActive("/dashboard") ? "nav-item active" : "nav-item"
+            }
+          >
+            <Link className="nav-link" to="/dashboard">
+              <span className="menu-title">
+                <Trans>Dashboard</Trans>
+              </span>
+              <i className="mdi mdi-home menu-icon"></i>
+            </Link>
+          </li>
+          <li
+            className={
+              this.isPathActive("/basic-ui") ? "nav-item active" : "nav-item"
+            }
+          >
+            <div
+              className={
+                this.state.basicUiMenuOpen
+                  ? "nav-link menu-expanded"
+                  : "nav-link"
+              }
+              onClick={() => this.toggleMenuState("basicUiMenuOpen")}
+              data-toggle="collapse"
+            >
+              <span className="menu-title">
+                <Trans>Booking</Trans>
+              </span>
+              <i className="menu-arrow"></i>
+              <i className="mdi mdi-crosshairs-gps menu-icon"></i>
+            </div>
+            <Collapse in={this.state.basicUiMenuOpen}>
+              <ul className="nav flex-column sub-menu">
+                <li className="nav-item">
+                  {" "}
+                  <Link
+                    className={
+                      this.isPathActive("/basic-ui/buttons")
+                        ? "nav-link active"
+                        : "nav-link"
+                    }
+                    to="/basic-ui/buttons"
+                  >
+                    <Trans>Trips</Trans>
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  {" "}
+                  <Link
+                    className={
+                      this.isPathActive("/basic-ui/dropdowns")
+                        ? "nav-link active"
+                        : "nav-link"
+                    }
+                    to="/basic-ui/dropdowns"
+                  >
+                    <Trans>Equipment</Trans>
+                  </Link>
+                </li>
+              </ul>
+            </Collapse>
+          </li>
+          <li
+            className={
+              this.isPathActive("/form-elements")
+                ? "nav-item active"
+                : "nav-item"
+            }
+          >
+            <div
+              className={
+                this.state.formElementsMenuOpen
+                  ? "nav-link menu-expanded"
+                  : "nav-link"
+              }
+              onClick={() => this.toggleMenuState("formElementsMenuOpen")}
+              data-toggle="collapse"
+            >
+              <span className="menu-title">
+                <Trans>Reports</Trans>
+              </span>
+              <i className="menu-arrow"></i>
+              <i className="mdi mdi-format-list-bulleted menu-icon"></i>
+            </div>
+            <Collapse in={this.state.formElementsMenuOpen}>
+              <ul className="nav flex-column sub-menu">
+                <li className="nav-item">
+                  {" "}
+                  <Link
+                    className={
+                      this.isPathActive("/form-elements/basic-elements")
+                        ? "nav-link active"
+                        : "nav-link"
+                    }
+                    to="/form-elements/basic-elements"
+                  >
+                    <Trans>Trips</Trans>
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  {" "}
+                  <Link
+                    className={
+                      this.isPathActive("/form-elements/basic-elements")
+                        ? "nav-link active"
+                        : "nav-link"
+                    }
+                    to="/form-elements/basic-elements"
+                  >
+                    <Trans>Equipment</Trans>
+                  </Link>
+                </li>
+              </ul>
+            </Collapse>
+          </li>
+          <li
+            className={
+              this.isPathActive("/tables") ? "nav-item active" : "nav-item"
+            }
+          >
+            <div
+              className={
+                this.state.tablesMenuOpen
+                  ? "nav-link menu-expanded"
+                  : "nav-link"
+              }
+              onClick={() => this.toggleMenuState("tablesMenuOpen")}
+              data-toggle="collapse"
+            >
+              <span className="menu-title">
+                <Trans>Weather</Trans>
+              </span>
+              <i className="menu-arrow"></i>
+              <i className="mdi mdi-table-large menu-icon"></i>
+            </div>
+            <Collapse in={this.state.tablesMenuOpen}>
+              <ul className="nav flex-column sub-menu">
+                <li className="nav-item">
+                  {" "}
+                  <Link
+                    className={
+                      this.isPathActive("/tables/basic-table")
+                        ? "nav-link active"
+                        : "nav-link"
+                    }
+                    to="/tables/basic-table"
+                  >
+                    <Trans>Basic Table</Trans>
+                  </Link>
+                </li>
+              </ul>
+            </Collapse>
+          </li>
+          <li
+            className={
+              this.isPathActive("/icons") ? "nav-item active" : "nav-item"
+            }
+          >
+            <div
+              className={
+                this.state.iconsMenuOpen ? "nav-link menu-expanded" : "nav-link"
+              }
+              onClick={() => this.toggleMenuState("iconsMenuOpen")}
+              data-toggle="collapse"
+            >
+              <span className="menu-title">
+                <Trans>Settings</Trans>
+              </span>
+              <i className="menu-arrow"></i>
+              <i className="mdi mdi-contacts menu-icon"></i>
+            </div>
+            <Collapse in={this.state.iconsMenuOpen}>
+              <ul className="nav flex-column sub-menu">
+                <li className="nav-item">
+                  {" "}
+                  <Link
+                    className={
+                      this.isPathActive("/icons/mdi")
+                        ? "nav-link active"
+                        : "nav-link"
+                    }
+                    to="/icons/mdi"
+                  >
+                    <Trans>Users</Trans>
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  {" "}
+                  <Link
+                    className={
+                      this.isPathActive("/icons/mdi")
+                        ? "nav-link active"
+                        : "nav-link"
+                    }
+                    to="/icons/mdi"
+                  >
+                    <Trans>Equipment</Trans>
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  {" "}
+                  <Link
+                    className={
+                      this.isPathActive("/icons/mdi")
+                        ? "nav-link active"
+                        : "nav-link"
+                    }
+                    to="/icons/mdi"
+                  >
+                    <Trans>Vehicles</Trans>
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  {" "}
+                  <Link
+                    className={
+                      this.isPathActive("/icons/mdi")
+                        ? "nav-link active"
+                        : "nav-link"
+                    }
+                    to="/icons/mdi"
+                  >
+                    <Trans>Drivers</Trans>
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  {" "}
+                  <Link
+                    className={
+                      this.isPathActive("/icons/mdi")
+                        ? "nav-link active"
+                        : "nav-link"
+                    }
+                    to="/tables/basic-table"
+                  >
+                    <Trans>Organisations</Trans>
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  {" "}
+                  <Link
+                    className={
+                      this.isPathActive("/icons/mdi")
+                        ? "nav-link active"
+                        : "nav-link"
+                    }
+                    to="/icons/mdi"
+                  >
+                    <Trans>Locations</Trans>
+                  </Link>
+                </li>
+              </ul>
+            </Collapse>
+          </li>
+          <li
+            className={
+              this.isPathActive("/charts") ? "nav-item active" : "nav-item"
+            }
+          >
+            <div
+              className={
+                this.state.chartsMenuOpen
+                  ? "nav-link menu-expanded"
+                  : "nav-link"
+              }
+              onClick={() => this.toggleMenuState("chartsMenuOpen")}
+              data-toggle="collapse"
+            >
+              <span className="menu-title">
+                <Trans>Blacklist</Trans>
+              </span>
+              <i className="menu-arrow"></i>
+              <i className="mdi mdi-chart-bar menu-icon"></i>
+            </div>
+            <Collapse in={this.state.chartsMenuOpen}>
+              <ul className="nav flex-column sub-menu">
+                <li className="nav-item">
+                  {" "}
+                  <Link
+                    className={
+                      this.isPathActive("/charts/chart-js")
+                        ? "nav-link active"
+                        : "nav-link"
+                    }
+                    to="/charts/chart-js"
+                  >
+                    <Trans>Chart Js</Trans>
+                  </Link>
+                </li>
+              </ul>
+            </Collapse>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+
+  isPathActive(path) {
+    return this.props.location.pathname.startsWith(path);
+  }
+
+  componentDidMount() {
+    this.onRouteChanged();
+    // add class 'hover-open' to sidebar navitem while hover in sidebar-icon-only menu
+    const body = document.querySelector("body");
+    document.querySelectorAll(".sidebar .nav-item").forEach((el) => {
+      el.addEventListener("mouseover", function () {
+        if (body.classList.contains("sidebar-icon-only")) {
+          el.classList.add("hover-open");
+        }
+      });
+      el.addEventListener("mouseout", function () {
+        if (body.classList.contains("sidebar-icon-only")) {
+          el.classList.remove("hover-open");
+        }
+      });
+    });
+  }
+}
+
+export default withRouter(Sidebar);
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./app/App";
+import "./i18n";
+import * as serviceWorker from "./serviceWorker";
+
+ReactDOM.render(
+  <BrowserRouter basename="/fleet_manager">
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
+
+serviceWorker.unregister();
